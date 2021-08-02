e78f297b0ba3114d8930592731da2cf5abf93222
Yury Vozleev <yury.vozleev@mobileup.ru>
2021-08-02 20:47:24 +0300

    Create project

---
+++ GBShop/GBShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GBShop
+//
+//  Created by Yury Vozleev on 02.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
